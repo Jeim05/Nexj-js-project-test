48072dad82ca8435e30f14e949de712ef4cd1add
Jeimmy Lopez Vega <lopezvegajeimmy@gmail.com>
2023-10-10 16:26:02 -0600

    Tech icons updated

--- src/components/dataProject.jsx
+++ src/components/dataProject.jsx
@@ -1,44 +1,19 @@
+import technologies from "./dataTech"
+
 const project = [
     {
         id: 1,
         name: "Proyecto 1",
         tech: [
-            {
-                id: 1,
-                name: "HTML",
-                icon: ['fa-brands', 'fa-html5'],
-                color: "text-[#E64C18]"
-            },
-            {
-                id: 2,
-                name: "CSS",
-                icon: ['fa-brands', 'fa-css3-alt'],
-                color: "text-yellow-500"
-            },
-            {
-                id: 3,
-                name: "JavaScript",
-                icon: ['fa-brands', 'fa-square-js'],
-                color: "text-white"
-            }
+            technologies.find((tech) => tech.name === "HTML"),
+            technologies.find((tech) => tech.name === "CSS"),
         ]
     },
     {
-        id: 2,
-        name: "Proyecto 1",
+        id: 1,
+        name: "Proyecto 2",
         tech: [
-            {
-                id: 1,
-                name:"CSS",
-                icon: ['fa-brands', 'fa-css3-alt'],
-                color: "#214CE5"
-            },
-            {
-                id: 2,
-                name:"Wordpress",
-                icon: ['fa-brands', 'fa-wordpress'],
-                color: "text-[#214CE5]"
-            },
+            technologies.find((tech) => tech.name === "Wordpress"),
         ]
     }
 ]
--- src/components/dataTech.jsx
+++ src/components/dataTech.jsx
@@ -1,32 +1,34 @@
-technologies = [
+const technologies = [
     {
         id: 1,
-        name:"HTML",
+        name: "HTML",
         icon: ['fa-brands', 'fa-html5'],
-        color: "[#E64C18]"
+        color: "text-[#E64C18]"
     },
     {
         id: 2,
-        name:"CSS",
+        name: "CSS",
         icon: ['fa-brands', 'fa-css3-alt'],
-        color: "[#214CE5]"
+        color: "text-blue-900"
     },
     {
         id: 3,
-        name:"Bootstrap",
-        icon: ['fa-brands', 'fa-bootstrap'],
-        color: "[#214CE5]"
+        name: "JavaScript",
+        icon: ['fa-brands', 'fa-square-js'],
+        color: "text-white"
     },
     {
         id: 4,
         name:"JavaScript",
         icon: ['fa-brands', 'fa-square-js'],
-        color: "[#214CE5]"
+        color: "text-yellow-500"
     },
     {
         id: 5,
         name:"Wordpress",
-        icon: ['fa-brands', 'fa-html5'],
-        color: "[#214CE5]"
+        icon: ['fa-brands', 'fa-wordpress'],
+        color: "text-white"
     },
 ]
+
+export default technologies
